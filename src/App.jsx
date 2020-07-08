710d67fd1ef183ced60004456f460ff253b6937e
Pouya Oftadeh <poftadeh2@gmail.com>
2020-07-08 17:34:39 -0400

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Search from './components/Search';
+import Heading from './components/Heading';
+
+const App = () => (
+  <>
+    <Heading />
+    <Search />
+  </>
+);
+
+export default App;
---
+++ src/components/Search/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <>
+      <input type="text" placeholder="City" />
+      <input type="text" placeholder="Refine" />
+    </>
+  );
+};
+
+export default Search;
