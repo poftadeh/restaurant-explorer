653f075fa6a7db6c65a11752eb7bae72520b0903
Pouya Oftadeh <poftadeh2@gmail.com>
2020-07-09 10:16:05 -0400

    test: complete async action tests

--- src/actions/__tests__/actions.test.js
+++ src/actions/__tests__/actions.test.js
@@ -1,21 +1,36 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import fetchMock from 'fetch-mock';
-import { getCityRestaurants } from '../index';
-import { setRefineText } from '../index';
-import { SET_REFINE_TEXT } from '../types';
+import { getCityRestaurants, setRefineText } from '../index';
+import { SET_REFINE_TEXT, GET_RESTAURANTS } from '../types';
+
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 describe('actions', () => {
-  it('should get restaurants from API', async () => {
-    const dummyData = {
-      restaurants: [{ name: 'foo', area: 'bar', address: 'baz', price: 3 }],
-    };
+  afterEach(() => {
+    fetchMock.restore();
+  });
 
-    const data = await getCityRestaurants('Toronto');
-    const json = await data.json();
-    expect(json).toBe(dummyData);
+  it('should properly create a GET_RESTAURANTS action', () => {
+    const mockedRestaurants = [
+      { name: 'foo', address: 'bar', area: 'baz', price: 3 },
+    ];
+
+    fetchMock.getOnce('/restaurants?city=Toronto', {
+      body: { restaurants: mockedRestaurants },
+      headers: { 'content-type': 'application/json' },
+    });
+
+    const expectedActions = [
+      { type: GET_RESTAURANTS, payload: mockedRestaurants },
+    ];
+
+    const store = mockStore({ restaurants: null });
+
+    return store.dispatch(getCityRestaurants('Toronto')).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
   });
 
   it('should create an action to set the refine text string', () => {
@@ -27,27 +42,3 @@ describe('actions', () => {
     expect(setRefineText(text)).toEqual(expectedAction);
   });
 });
-
-describe('async actions', () => {
-  afterEach(() => {
-    fetchMock.restore()
-  })
-
-  it('creates FETCH_TODOS_SUCCESS when fetching todos has been done', () => {
-    fetchMock.getOnce('/todos', {
-      body: { todos: ['do something'] },
-      headers: { 'content-type': 'application/json' }
-    })
-
-    const expectedActions = [
-      { type: types.FETCH_TODOS_REQUEST },
-      { type: types.FETCH_TODOS_SUCCESS, body: { todos: ['do something'] } }
-    ]
-    const store = mockStore({ todos: [] })
-
-    return store.dispatch(actions.fetchTodos()).then(() => {
-      // return of async actions
-      expect(store.getActions()).toEqual(expectedActions)
-    })
-  })
-})
