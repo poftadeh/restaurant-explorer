e595a054b0e75d4a3a3f19daaa4b580aeb6d06f8
Pouya Oftadeh <poftadeh2@gmail.com>
2020-07-09 16:38:52 -0400

    test: add Heading snapshot

--- src/components/Heading/__tests_/Heading.test.js
+++ src/components/Heading/__tests_/Heading.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Heading from '../index';
 
-it('renders Heading title message properly', () => {
+it('renders <Heading /> properly', () => {
   const wrapper = shallow(<Heading />);
-  const h1 = <h1 className="main-heading">Restaurant Explorer</h1>;
-  expect(wrapper.contains(h1)).toEqual(true);
+  expect(wrapper).toMatchSnapshot();
 });
